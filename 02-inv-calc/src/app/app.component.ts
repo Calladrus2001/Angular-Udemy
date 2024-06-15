473c9ec8a4f01c2eba51210acdd9c101184d870b
Calladrus2001 <dugarvishesh@gmail.com>
2024-06-15 19:07:14 +0530

    feat: 02-inv-calc form handling

--- 02-inv-calc/src/app/app.component.ts
+++ 02-inv-calc/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { GridInputComponent } from './components/grid-input/grid-input.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GridInputComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = '02-inv-calc';
+  init_investment: number = 0;
+  annual_investment: number = 0;
+  exp_return: number = 0;
+  duration: number = 0;
+  
+  calculate(init_investment:number, annual_investment:number, exp_return: number, duration:number) { 
+    console.log(`${init_investment}, ${annual_investment}, ${exp_return}, ${duration}`)
+  }
 }
--- 02-inv-calc/src/app/components/grid-input/grid-input.component.ts
+++ 02-inv-calc/src/app/components/grid-input/grid-input.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-grid-input',
   standalone: true,
-  imports: [],
   templateUrl: './grid-input.component.html',
 })
 export class GridInputComponent {
+  @Input() displayText!: string;
+  @Input() value!: number;
+  @Output() valueChange = new EventEmitter<number>();
 
+  onValueChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.valueChange.emit(Number(inputElement.value));
+  }
 }
