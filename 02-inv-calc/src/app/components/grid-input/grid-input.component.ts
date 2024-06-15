6b133125cd707263e84b71bed55e38a768fa9e38
Calladrus2001 <dugarvishesh@gmail.com>
2024-06-15 17:00:01 +0530

    feat: 02-inv-calc init

---
+++ 02-inv-calc/src/app/components/grid-input/grid-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './grid-input.component.html',
+})
+export class GridInputComponent {
+
+}
